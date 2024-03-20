9398fc18f77048e8e8e8b1228ba5d8c42b915428
rrahul08 <thefirstclass555@gmail.com>
2024-03-20 14:45:13 +0530

    enhanced api using pathvariable

--- src/main/java/com/rahul/rest/webservices/restfulwebservices/hello/HelloWorld.java
+++ src/main/java/com/rahul/rest/webservices/restfulwebservices/hello/HelloWorld.java
@@ -2,6 +2,7 @@ package com.rahul.rest.webservices.restfulwebservices.hello;
 
 import com.rahul.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,12 @@ public class HelloWorld {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloworldbean(){
         return new HelloWorldBean("Hello World!");
+    }
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloworldpathvariable(@PathVariable String name){
+
+
+        return new HelloWorldBean(String.format("Hello , %s ",name));
     }
 }
