5f77d6b66ccdf6d5f04c88662514e16caad9529c
rrahul08 <thefirstclass555@gmail.com>
2024-03-20 13:00:49 +0530

    added helloworldbean

--- src/main/java/com/rahul/rest/webservices/restfulwebservices/hello/HelloWorld.java
+++ src/main/java/com/rahul/rest/webservices/restfulwebservices/hello/HelloWorld.java
@@ -1,5 +1,6 @@
 package com.rahul.rest.webservices.restfulwebservices.hello;
 
+import com.rahul.rest.webservices.restfulwebservices.helloworld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class HelloWorld {
     @GetMapping(path = "/hello-world")
     public String helloworld(){
         return "Hello World!";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloworldbean(){
+        return new HelloWorldBean("Hello World!");
     }
 }
