309f4c4c51ce4791d951453a528d97c53abc4868
rrahul08 <thefirstclass555@gmail.com>
2024-03-20 15:34:33 +0530

    implementing get method for user resource

--- src/main/java/com/rahul/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/rahul/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -1,17 +1,34 @@
 package com.rahul.rest.webservices.restfulwebservices.user;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
 
+@Component
 public class UserDaoService {
+// JPA/Hibernate > Database
+    // UserDaoService > Static List
 
     private static List<User> users = new ArrayList<>();
 
     static {
-        users.add (new User(1,"Rahul", LocalDate.now().minusYears(22)));
-        users.add (new User(2,"Sriram", LocalDate.now().minusYears(23)));
-        users.add (new User(3,"Paramesh", LocalDate.now().minusYears(22)));
+        users.add(new User(1,"Adam",LocalDate.now().minusYears(30)));
+        users.add(new User(2,"Eve",LocalDate.now().minusYears(25)));
+        users.add(new User(3,"Jim",LocalDate.now().minusYears(20)));
+    }
 
+    public List<User> findAll() {
+        return users;
+    }
+
+    //public User save(User user) {
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId() == id;
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
