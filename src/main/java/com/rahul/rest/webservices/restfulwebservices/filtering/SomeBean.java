b270f027ef8180cb7f2a931e6133b9238393300f
rrahul08 <thefirstclass555@gmail.com>
2024-03-21 16:54:35 +0530

    implemented dynamic filtering

--- src/main/java/com/rahul/rest/webservices/restfulwebservices/filtering/SomeBean.java
+++ src/main/java/com/rahul/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -1,11 +1,13 @@
 package com.rahul.rest.webservices.restfulwebservices.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 
     private String field1;
-    @JsonIgnore
+
     private String field2;
     private String field3;
 
