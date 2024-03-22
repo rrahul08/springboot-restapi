d1c0422c7d10df358832e1fb75c8e6873e54fd08
rrahul08 <thefirstclass555@gmail.com>
2024-03-22 11:29:31 +0530

    created post entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,12 @@ values (1002,current_date(),'abc');
 
 insert into user_details(id,birth_date,name)
 values (1003,current_date(),'xyz');
+
+insert into post(id,description,user_id)
+values (2001,'Learn course AWS',1001);
+
+insert into post(id,description,user_id)
+values (2002,'Learn course Webdev',1002);
+
+insert into post(id,description,user_id)
+values (2003,'Learn course Ml',1003);
